d68b6f006f40d2fa490e2e256881ff89685dae34
AlexNav73 <fifthhorseman@mail.ru>
2017-11-17 21:48:35 +0200

    Refactored code a little bit and start working on text parsing

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,68 @@ extern crate glutin;
 extern crate imgui;
 extern crate imgui_gfx_renderer;
 extern crate hound;
+extern crate regex;
+#[macro_use]
+extern crate lazy_static;
 
 mod song;
 mod support_gfx;
 
 use imgui::*;
+use glutin::WindowEvent;
+use regex::Regex;
 
 use song::Song;
+use support_gfx::Program;
+
+lazy_static! {
+    static ref SPLIT: Regex = Regex::new(r##"\[([0-9:]+)\]([^0-9:]*)\[([0-9:]+)\]"##).unwrap();
+}
 
 struct State {
+    title: String,
     lyrics: ImString
 }
 
 impl Default for State {
     fn default() -> Self {
         State {
+            title: "melos".to_owned(),
             lyrics: ImString::with_capacity(2000)
+        }
+    }
+}
+
+impl Program for State {
+    fn title(&self) -> &str {
+        self.title.as_str()
+    }
+
+    fn show<'a>(&mut self, ui: &Ui<'a>) -> bool {
+        let mut opened = true;
+        ui.window(im_str!("Hello world"))
+            .size((420.0, 565.0), ImGuiCond::Once)
+            .opened(&mut opened)
+            .build(|| {
+                ui.input_text(im_str!(""), &mut self.lyrics)
+                    .multiline(ImVec2::new(400.0, 530.0))
+                    .build();
+            });
+        opened
+    }
+
+    fn on_event(&mut self, event: WindowEvent) {
+        match event {
+            WindowEvent::ReceivedCharacter(c) => {
+                for captures in  SPLIT.captures_iter(self.lyrics.to_str()) {
+                    let start = captures.get(1).map_or("", |m| m.as_str());
+                    let text = captures.get(2).map_or("", |m| m.as_str());
+                    let end = captures.get(3).map_or("", |m| m.as_str());
+
+                    println!("START: {:?}\nTEXT: {:?}\nEND: {:?}", start, text, end);
+                }
+            },
+            _ => {}
         }
     }
 }
@@ -29,23 +75,7 @@ impl Default for State {
 fn main() {
     //let mut song = Song::new("samples/sonne.wav");
     //song.split_by(&[(27, 55), (84, 115)], "samples/sonna.wav");
-    let mut state = State::default();
 
-    support_gfx::run(
-        "melos".to_owned(),
-        |ui| show(ui, &mut state));
-}
-
-fn show<'a>(ui: &Ui<'a>, state: &mut State) -> bool {
-    let mut opened = true;
-    ui.window(im_str!("Hello world"))
-        .size((100.0, 100.0), ImGuiCond::FirstUseEver)
-        .opened(&mut opened)
-        .build(|| {
-            ui.input_text(im_str!("Lyrics"), &mut state.lyrics)
-              .multiline(ImVec2::new(500.0, 500.0))
-              .build();
-        });
-    opened
+    State::default().run();
 }
 
