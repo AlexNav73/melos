f44cb8dca9b283383c26641f2e5a8306d855ef53
Aliaksandr_Navitski <Aliaksandr_Navitski@epam.com>
2018-03-28 13:00:52 +0300

    Implement file selection

--- src/dialogs.rs
+++ src/dialogs.rs
@@ -8,6 +8,8 @@ pub struct OpenFileDialog {
     pub opened: bool,
     load: bool,
     path: ImString,
+    cached_paths: Vec<ImString>,
+    selected_item: i32,
     state: State
 }
 
@@ -20,11 +22,13 @@ impl AppContext for OpenFileDialog {
                 .opened(&mut opened)
                 .collapsible(false)
                 .build(|| {
-                    ui.input_text(im_str!("path"), &mut self.path)
+                    ui.input_text(im_str!("##path"), &mut self.path)
                         .build();
+                    ui.same_line(0.0);
                     if ui.button(im_str!("open"), (0.0, 0.0)) {
                         self.load = self.state.open(self.path.to_str());
                     }
+                    self.show_file_browser(ui);
                 });
         }
         self.opened = !(self.load || !opened);
@@ -38,6 +42,8 @@ impl OpenFileDialog {
             opened: false,
             load: false,
             path: ImString::with_capacity(256),
+            cached_paths: Vec::new(),
+            selected_item: 0,
             state
         }
     }
@@ -48,6 +54,32 @@ impl OpenFileDialog {
             return true;
         }
         false
+    }
+
+    fn show_file_browser<'a>(&mut self, ui: &Ui<'a>) {
+        if self.cached_paths.is_empty() {
+            self.cached_paths = enumerate_files();
+        }
+
+        let old_selection = self.selected_item;
+        {
+            let rpath = self.cached_paths.iter()
+                .map(|x| x.as_ref())
+                .collect::<Vec<_>>();
+
+            if ui.list_box(im_str!("##files"),
+                &mut self.selected_item,
+                rpath.as_slice(), 5)
+            {
+            }
+        }
+
+        if let Some(ref p) = self.cached_paths.get(self.selected_item as usize) {
+            if old_selection != self.selected_item {
+                self.path.clear();
+                self.path.push_str(p.as_ref());
+            }
+        }
     }
 }
 
@@ -86,4 +118,8 @@ impl SaveFileDialog {
         self.opened = !(self.saved || !opened);
         self.opened
     }
+}
+
+fn enumerate_files() -> Vec<ImString> {
+    vec![]
 }
