6d9d9cfd06afd9ada307add7cbc453c1b50fa73b
AlexNav73 <fifthhorseman@mail.ru>
2018-04-22 19:17:10 +0300

    Update build (CI) scripts

--- ci/script.sh
+++ ci/script.sh
@@ -11,13 +11,6 @@ main() {
         return
     fi
 
-    #cross test --target $TARGET
-    #cross test --target $TARGET --release
-
-    #cross run --target $TARGET
-    #cross run --target $TARGET --release
-}
-
 # we don't run the "test phase" when doing deploys
 if [ -z $TRAVIS_TAG ]; then
     main
